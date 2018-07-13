f79d198de21e1f7b32736193be6cecdf1d8d7250
David Ehlen <dehlen@me.com>
2018-07-13 16:49:45 +0200

    push to remote after version bump if remote was configured

--- templater.js
+++ templater.js
@@ -2,6 +2,7 @@ const path = require('path')
 const moment = require('moment')
 
 const rename = require('./rename')
+const stringUtil = require('./stringUtil')
 
 const removeTrailingDot = (string) => {
     return string.replace(/\.$/, '');
@@ -18,6 +19,14 @@ const swiftlintLane = (shouldBeAdded) => {
     }
 }
 
+const gitPushConfiguration = (githubURL) => {
+    if (stringUtil.isEmpty(githubURL)) {
+        return ""
+    } else {
+        return "push_to_git_remote"
+    }
+}
+
 exports.copyTemplateProject = async (projectLocation, name, configuration) => {
     const replacementMap = {
         '{PROJECT_NAME}': name,
@@ -27,7 +36,8 @@ exports.copyTemplateProject = async (projectLocation, name, configuration) => {
         '{ORGANIZATION}': configuration.organization,
         '{BUNDLE_IDENTIFIER_PREFIX}': removeTrailingDot(configuration.bundleIdPrefix),
         '{DEPLOYMENT_TARGET}': configuration.deploymentTarget,
-        '{SWIFTLINT_LANE}': swiftlintLane(configuration.swiftlint)
+        '{SWIFTLINT_LANE}': swiftlintLane(configuration.swiftlint),
+        '{GIT_PUSH_CONFIGURATION}': gitPushConfiguration(configuration.githubURL),
     }
     await rename.recusivelyCopy(path.join(__dirname, 'Template'), projectLocation, replacementMap, configuration)
 }
