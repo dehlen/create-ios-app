12657cc0abc77bded8a7706dde339febfdde8ebe
David Ehlen <dehlen@me.com>
2018-07-28 21:07:34 +0200

    update carthage scripts to use mint

--- Template/plugins/scripts/build-dependencies.sh
+++ Template/plugins/scripts/build-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PROJECT_ROOT=$(dirname $(dirname "$0"))
 echo "Updating Carthage dependencies"
-cd $PROJECT_ROOT && carthage update --platform iOS --no-use-binaries --cache-builds
+cd $PROJECT_ROOT && mint run carthage carthage update --platform iOS --no-use-binaries --cache-builds
 echo "Finished updating Carthage dependencies"
