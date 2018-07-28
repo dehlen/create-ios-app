647e6aef975a6980fb9b5d848dc813a0338adfe4
David Ehlen <dehlen@me.com>
2018-07-28 21:08:14 +0200

    added Strings replacement file when not using swiftgen
    if swiftgen is not installed by the user through the setup the app would otherwise get compile errors since the L10n enum would not be available

---
+++ Template/plugins/Strings.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// swiftlint:disable explicit_type_interface identifier_name line_length nesting type_body_length type_name
+public enum L10n {
+  /// About
+  public static let about = L10n.tr("Localizable", "about")
+  /// Home
+  public static let home = L10n.tr("Localizable", "home")
+  /// Used libraries
+  public static let libraries = L10n.tr("Localizable", "libraries")
+}
+// swiftlint:enable explicit_type_interface identifier_name line_length nesting type_body_length type_name
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {}
